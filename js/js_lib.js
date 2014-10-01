a66a0572a47e5724918ee40b71213ec781f42382
Jesper Martinussen <jesperkmartinussen@gmail.com>
2014-10-01 16:33:52 +0200

    final indent fix

--- js/js_lib.js
+++ js/js_lib.js
@@ -1,29 +1,29 @@
 //Navigation Script
 $(function() {  
-	var pull = $('#pull');  
-	menu = $('nav ul');  
-	menuHeight = menu.height();  
+    var pull = $('#pull');  
+    menu = $('nav ul');  
+    menuHeight = menu.height();  
 		  
-	$(pull).on('click', function(e) {  
-		e.preventDefault();  
-		menu.slideToggle();  
-		});  
-	});
+    $(pull).on('click', function(e) {  
+        e.preventDefault();  
+        menu.slideToggle();  
+        });  
+    });
 
-	$(window).resize(function(){  
-	var w = $(window).width();  
-	
-	if(w > 320 && menu.is(':hidden')) {  
-		menu.removeAttr('style');  
-	}  
+    $(window).resize(function(){  
+    var w = $(window).width();  
+
+    if(w > 320 && menu.is(':hidden')) {  
+        menu.removeAttr('style');  
+    }  
 });
 
-//Smooth Scroll 
+//Smooth Scroll.
 
 $(function() {
-	var $root = $('html, body');
+    var $root = $('html, body');
 		
-	$('a').click(function() {
+    $('a').click(function() {
     var href = $.attr(this, 'href');
     	
     $root.animate({
@@ -33,5 +33,5 @@ $(function() {
     });
     
     return false;
-	});
+    });
 });
