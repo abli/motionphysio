1c14f38bb46f2ffd9ea677d59e2794116e252b38
Jesper Martinussen <jesperkmartinussen@gmail.com>
2014-10-01 13:07:52 +0200

    adding responsive navigation

---
+++ js/js_lib.js
@@ -0,0 +1,37 @@
+//Navigation Script
+$(function() {  
+	var pull = $('#pull');  
+	menu = $('nav ul');  
+	menuHeight = menu.height();  
+		  
+	$(pull).on('click', function(e) {  
+		e.preventDefault();  
+		menu.slideToggle();  
+		});  
+	});
+
+	$(window).resize(function(){  
+	var w = $(window).width();  
+	
+	if(w > 320 && menu.is(':hidden')) {  
+		menu.removeAttr('style');  
+	}  
+});
+
+//Smooth Scroll 
+
+$(function() {
+	var $root = $('html, body');
+		
+	$('a').click(function() {
+    var href = $.attr(this, 'href');
+    	
+    $root.animate({
+    scrollTop: $(href).offset().top - 80
+    }, 800, function () {
+    window.location.hash = href;
+    });
+    
+    return false;
+	});
+});
