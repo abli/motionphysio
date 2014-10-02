85c13bd2dbfcbd4131e9043a7c612b2c0d7b72df
dyrehauge <dthomasn@gmail.com>
2014-10-02 11:28:15 +0200

    fixed indentation

--- index.php
+++ index.php
@@ -1,72 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Motionpsysio</title>
-	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Motionpsysio</title>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="grid grid-pad">
-    <div class="col-1-1">
-       <div class="content">
-           <video autoplay="autoplay" loop>
-						<source src="video/polina.webm" type="video/webm">
-						<source src="video/polina.ogg" type="video/ogg">
-						Your browser does not support the video tag.
-					</video>
-       </div>
+    <div class="grid grid-pad">
+        <div class="col-1-1">
+           <div class="content">
+               <video autoplay="autoplay" loop>
+                    <source src="video/polina.webm" type="video/webm">
+                    <source src="video/polina.ogg" type="video/ogg">
+                    Your browser does not support the video tag.
+                </video>
+           </div>
+        </div>
     </div>
-</div>
-	<div class="seperator"></div>
-	<div class="grid grid-pad">
-	    <div class="col-1-1">
-	       <div class="content">
-	           <div class="content clearfix">
-	<h1>Some Headline here</h1>
-	<img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-	<h2>some secondary headline here</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	
-	<a href="#">Link to something</a>
+    <div class="seperator"></div>
+    <div class="grid grid-pad">
+        <div class="col-1-1">
+           <div class="content">
+                <div class="content clearfix">
+                    <h1>Some Headline here</h1>
+                    <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
+                    <h2>some secondary headline here</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-	</div>
-	       </div>
-	    </div>
-	</div>
+                    <a href="#">Link to something</a>
 
-		<div class="seperator"></div>
-		<div class="grid grid-pad">
-    <div class="col-1-3">
-       <div class="content">
-       	<img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-           <h1>Experience Map</h1>
-           <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
-       </div>
+                </div>
+           </div>
+        </div>
     </div>
 
-    <div class="col-1-3">
-       <div class="content">
-          <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-           <h1>Experience Map</h1>
-           <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
-       </div>
-    </div>
-    <div class="col-1-3">
-       <div class="content">
-          <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-           <h1>Experience Map</h1>
-           <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
-       </div>
-    </div>
-</div>
-	</div>	
+    <div class="seperator"></div>
+    <div class="grid grid-pad">
+        <div class="col-1-3">
+           <div class="content">
+               <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
+               <h1>Experience Map</h1>
+               <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
+           </div>
+        </div>
 
+        <div class="col-1-3">
+           <div class="content">
+               <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
+               <h1>Experience Map</h1>
+               <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
+           </div>
+        </div>
+
+        <div class="col-1-3">
+           <div class="content">
+              <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
+               <h1>Experience Map</h1>
+               <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
+           </div>
+        </div>
+    </div>
 </body>
 </html>
