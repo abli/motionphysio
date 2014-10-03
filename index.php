36499ef9e5c704dad8528c917fad1440588e355f
Jesper Martinussen <jesperkmartinussen@gmail.com>
2014-10-03 17:26:37 +0200

    merged everything so far

--- index.php
+++ index.php
@@ -1,65 +1,178 @@
 <?php require "header.php"; ?>
 
-<div class="grid grid-pad">
-    <div class="col-1-1">
-        <div class="content">
-            <video autoplay="autoplay" loop>
-                <source src="video/polina.webm" type="video/webm">
-                <source src="video/polina.ogg" type="video/ogg">
-                Your browser does not support the video tag.
-            </video>
+<section id="home">
+    <video autoplay="autoplay" loop>
+        <source src="video/polina.webm" type="video/webm">
+        <source src="video/polina.ogg" type="video/ogg">
+        Your browser does not support the video tag.
+    </video>
+
+    <div class="grid grid-pad">
+        <div class="col-1-1">
+            <div class="content">
+                <div class="content clearfix">
+                    <h1>MOTION PHYSIO</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-1-2">
+           <div class="content">
+               <img alt="" src="http://placehold.it/500x350/0eafff/ffffff.png" />
+           </div>
+        </div>
+
+        <div class="col-1-2">
+           <div class="content">
+                <img id="image" alt="placeholder" src="http://placehold.it/500x175">
+                <img id="image" alt="placeholder" src="http://placehold.it/500x175">
+           </div>
         </div>
     </div>
-</div>
+</section>
 
 <div class="seperator"></div>
 
-<div class="grid grid-pad">
-    <div class="col-1-1">
-        <div class="content">
-            <div class="content clearfix">
-                <h1>Some Headline here</h1>
-                <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-                <h2>some secondary headline here</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                <a href="#">Link to something</a>
+<section id="products">
+    <div class="grid grid-pad">
+        <div class="col-1-1">
+            <div class="content">
+                <h1>Products</h1>
+                    <p id="lorem">Lorem ipsum dolor sit amet, maiores ornare ac fermentum, imperdiet ut vivamus a, nam lectus at
+                                  nunc. Quameuismod sem, semper ut potenti pellentesque quisque. In eget sapien sed, sit duis
+                                  vestibulum ultricies, placerat morbi amet vel, nullam in in lorem vel.</p>
+                    <h2>Experience map</h2>
+                    <img id="image" alt="placeholder" src="http://placehold.it/920x400">
             </div>
         </div>
     </div>
-</div>
+
+    <div class="grid grid-pad">
+        <h2>Listed products</h2>
+        <div class="col-1-3">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/320x200">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut iusto hic magni architecto, aliquid mollitia repellendus atque aut dolore, officiis voluptas, nam ipsa, veritatis quisquam laborum! Unde ipsum facere, cumque.</p>
+           </div>
+        </div>
+        <div class="col-1-3">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/320x200">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut iusto hic magni architecto, aliquid mollitia repellendus atque aut dolore, officiis voluptas, nam ipsa, veritatis quisquam laborum! Unde ipsum facere, cumque.</p>
+           </div>
+        </div>
+        <div class="col-1-3">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/320x200">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut iusto hic magni architecto, aliquid mollitia repellendus atque aut dolore, officiis voluptas, nam ipsa, veritatis quisquam laborum! Unde ipsum facere, cumque.</p>
+           </div>
+        </div>
+    </div>
+
+     <div class="grid grid-pad">
+        <div class="col-1-3">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/320x200">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut iusto hic magni architecto, aliquid mollitia repellendus atque aut dolore, officiis voluptas, nam ipsa, veritatis quisquam laborum! Unde ipsum facere, cumque.</p>
+           </div>
+        </div>
+        <div class="col-1-3">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/320x200">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut iusto hic magni architecto, aliquid mollitia repellendus atque aut dolore, officiis voluptas, nam ipsa, veritatis quisquam laborum! Unde ipsum facere, cumque.</p>
+           </div>
+        </div>
+        <div class="col-1-3">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/320x200">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut iusto hic magni architecto, aliquid mollitia repellendus atque aut dolore, officiis voluptas, nam ipsa, veritatis quisquam laborum! Unde ipsum facere, cumque.</p>
+           </div>
+        </div>
+    </div>
+</section>
 
 <div class="seperator"></div>
 
-<div class="grid grid-pad">
-    <div class="col-1-3">
-        <div class="content">
-            <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-            <h1>Experience Map</h1>
-            <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
+<section id="about">
+    <div class="grid grid-pad">
+        <h1>About Motion Physio</h1>
+        <div class="col-1-1">
+           <div class="content">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aliquid corporis repudiandae deserunt ut voluptatem similique, architecto distinctio, iure assumenda, accusantium magni fuga maxime a numquam sit laboriosam pariatur. Odio. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates architecto numquam voluptatem quia fugit sequi facilis quibusdam cum eum officia rerum fugiat, vitae dolorem ut odit ea hic ad deleniti</p>
+           </div>
         </div>
     </div>
 
-    <div class="col-1-3">
-        <div class="content">
-            <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-            <h1>Experience Map</h1>
-            <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
+    <div class="grid grid-pad">
+        <div class="col-1-2">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/550x250">
+           </div>
+        </div>
+        <div class="col-1-2">
+           <div class="content">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt quis veniam expedita velit quae error, at ducimus perspiciatis consequuntur, omnis et asperiores cumque! A amet porro voluptatibus ducimus! Explicabo, cumque.</p>
+               <button>Click me</button>
+           </div>
         </div>
     </div>
 
-    <div class="col-1-3">
-        <div class="content">
-            <img alt="" src="http://placehold.it/350x250/0eafff/ffffff.png" />
-            <h1>Experience Map</h1>
-            <p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quam consectetur deserunt delectus, maxime natus aperiam libero molestiae beatae, harum quidem sint repellendus adipisci explicabo quos maiores iusto alias mollitia.</p>
+      <div class="grid grid-pad">
+        <div class="col-1-2">
+           <div class="content">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt quis veniam expedita velit quae error, at ducimus perspiciatis consequuntur, omnis et asperiores cumque! A amet porro voluptatibus ducimus! Explicabo, cumque.</p>
+               <button>Click me</button>
+           </div>
+        </div>
+        <div class="col-1-2">
+           <div class="content">
+               <img id="image" alt="placeholder" src="http://placehold.it/550x250">
+           </div>
         </div>
     </div>
-</div>
+</section>
+
+<div class="seperator"></div>
+
+<section id="contact">
+    <div class="grid grid-pad">
+        <h1>Contact</h1>
+        <div class="col-1-2">
+           <div class="content">
+                <form action="" class="form" method="post" autocomplete="off">
+
+                    <fieldset>
+                        <input type="text" name="name" id="name" placeholder="Name" />
+                    </fieldset>
+                
+                    <fieldset>
+                        <input type="text" name="email" id="email" placeholder="Email@example.com" />
+                    </fieldset>
+                
+                    <fieldset>
+                        <input type="text" name="subject" id="subject" placeholder="Subject" />
+                    </fieldset>
+
+                    <textarea name="text" id="detail" placeholder="Insert the message here"></textarea>
+                    <input type="submit" value="Submit" id="submit"/>
+                </form>
+           </div>
+        </div>
+
+        <div class="col-1-2">
+           <div class="content">
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quia? Quisquam temporibus iusto laboriosam! Dicta impedit nostrum facere officia eveniet consectetur amet saepe, quibusdam quas labore rerum modi atque, maiores.</p>
+               <img id="image" alt="placeholder" src="http://placehold.it/550x250">
+           </div>
+        </div>
+    </div>
+</section>
 
 <?php require "footer.php"; ?>
